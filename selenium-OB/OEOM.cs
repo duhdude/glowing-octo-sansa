bf7884d7f8c6823fc39ba4b811b05becd68990ba
Ethan Dutton <ethan.dutton@orchardbrands.com>
2015-07-14 11:19:27 -0400

    Updated VS C# Proj

---
+++ selenium-OB/OEOM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+
+namespace selenium_OB
+{
+    [TestClass]
+    public class OEOM
+    {
+        IWebDriver driver = new ChromeDriver("C:/Users/Admin/Documents/ChromeDriver");
+
+        [TestMethod]
+        public void TC1_Login()
+        {
+            driver.Navigate().GoToUrl("https://gui-cp-qa.orchardbrands.biz:8080");
+        }
+    }
+}
